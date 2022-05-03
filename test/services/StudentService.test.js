d9908e9b4d39ac367d285b5fd6ce6b02acca14bf
EddYam <edder_yamir@hotmail.com>
2022-05-02 23:31:41 -0500

    Agregando prueba para consultar todos los estudiantes y corrigiendo valores esperados

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,13 +2,17 @@ const Reader = require("../../lib/utils/Reader");
 const StudentService = require("../../lib/services/StudentService");
 
 describe("Pruebas de unidad para la clase StudentService", () => {   
-    const students = Reader.readJsonFile("test/visualPartnersDB_test.json");    
+    const students = Reader.readJsonFile("test/visualPartnersDB_test.json");  
+    test("Prueba para consultar todos los estudiantes", () => {
+        const result = StudentService.getStudents(students);
+        expect(result.length).toBe(5)
+    });
     test("Prueba para consultar los emails de todos los estudiantes que tengan certificacion.", () => {
         const result = StudentService.getStudentsWithCertification(students);
-        expect(result).toBe(2); 
+        expect(result.length).toBe(4); 
     });
     test("Prueba para consultar todos los estudiantes que tengan credits mayor a 500", () => {
-        const result = StudentService.getStudentsApproved(students);
-        expect(result).toBe(3);
+        const result = StudentService.getStudentsCreditsValidation(students);
+        expect(result.length).toBe(3);
     });
 });
