ef35628e06d8bbb2fd783c067e1b790f5b83f3a7
EddYam <edder_yamir@hotmail.com>
2022-05-03 00:07:54 -0500

    Actualizando valores esperados

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -3,16 +3,16 @@ const StudentController = require("../../lib/controllers/StudentController");
 describe("Pruebas de unidad para la clase StudentController", () => {
     test("Prueba para consultar todos los estudiantes", () => {
         const result = StudentController.getStudents();
-        expect(result.length).toBe(5);
+        expect(result.length).toBe(51);
     });
 
     test("Prueba para consultar los emails de todos los estudiantes que tengan certificacion.", () => {
         const result = StudentController.getStudentsWithCertification();
-        expect(result.length).toBe(4)
+        expect(result.length).toBe(29)
     });
 
     test("Prueba para consultar todos los estudiantes que tengan credits mayor a 500", () => {
         const result = StudentController.getStudentsCreditsValidation();
-        expect(result.length).toBe(3);
+        expect(result.length).toBe(27);
     });
 });
