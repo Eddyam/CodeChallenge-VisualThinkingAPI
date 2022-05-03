06381699ab72748a8af9334045a4bc9d04209bb5
EddYam <edder_yamir@hotmail.com>
2022-05-02 21:22:34 -0500

    Corrigiendo path de Reader.test.js

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,9 @@
-const Reader = require("../../lib/Reader");
+const Reader = require("../../lib/utils/Reader");
 
 describe("Pruebas de unidad para la clase Reader", () => {
     test("Verificar la lectura del archivo", () => {
-        const students = Reader.readJsonFile("test/visualPartners.json");
-        expect(explorers).toBeTruthy();
+        const students = Reader.readJsonFile("test/visualPartnersDB_test.json");       
+        expect(students).toBeTruthy();
+        expect(students.length).toBe(5);
     });
 });
